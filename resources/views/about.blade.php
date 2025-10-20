4db7f5582fb46f29b1da1471a8a2f5255b7e5809
afdikk <afrisyadwiky@gmail.com>
2025-10-20 07:41:31 +0700

    header perbaikan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,24 @@
 @section('content')
 @extends('components.header')
 
+ {{-- CONTENT --}}
+  <main>
+    {{-- Flash message (opsional) --}}
+    @if(session('status'))
+      <div class="container pt-4">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          {{ session('status') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </div>
+    @endif
+
+    @yield('content')
+  </main>
+
+    {{-- LOGIN MODAL (selalu tersedia di layout) --}}
+  @include('components.login-modal')
+
 @include('components.about.hero')
 @include('components.about.cerita')
 @include('components.about.filosofi')
